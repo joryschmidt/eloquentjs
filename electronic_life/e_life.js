26244bd54d6c6095820160a3e415b55633671331
Jory Schmidt <joryschmidt1988@gmail.com>
2015-07-10 19:44:14 -0400

    grid prototype additions

--- electronic_life/e_life.js
+++ electronic_life/e_life.js
@@ -19,13 +19,24 @@ function Vector(x, y) {
 }
 
 function Grid(width, height){
-	this.space = function(width, height){
-		var arr = new Array();
-		for(i=0; i<height; i++){
-			arr.push(new Array(width));
-		}
-		return arr;
-	}
+	this.space = new Array(width * height);
 	this.width = width;
 	this.height = height;
 }
+
+Grid.prototype.isInside = function(vector){
+	return vector.x >= 0 && vector.x < this.width &&
+		   vector.y >= 0 && vector.y < this.height;
+}
+
+Grid.prototype.get = function(vector){
+	return this.space[vector.x + vector.y * this.width];
+}
+Grid.prototype.set = function(vector, value){
+	this.space[vector.y * this.width + vector.x] = value;
+}
+
+var grid = new Grid(5,5);
+console.log(grid.get(new Vector(1,1)));
+grid.set(new Vector(1,1), "X");
+console.log(grid.get(new Vector(1,1)));
