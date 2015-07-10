3a46ef849758a32ad328388b31faa44b8ac732c3
Jory Schmidt <joryschmidt1988@gmail.com>
2015-07-10 18:44:00 -0400

    2d grid definition

--- electronic_life/e_life.js
+++ electronic_life/e_life.js
@@ -12,3 +12,20 @@ var plan = ["############################",
             "# o  #         o       ### #",
             "#    #                     #",
             "############################"];
+
+function Vector(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+function Grid(width, height){
+	this.space = function(width, height){
+		var arr = new Array();
+		for(i=0; i<height; i++){
+			arr.push(new Array(width));
+		}
+		return arr;
+	}
+	this.width = width;
+	this.height = height;
+}
