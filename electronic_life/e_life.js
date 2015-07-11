915d6e49b523af8dd3f7718523974f288e9ec495
Jory Schmidt <joryschmidt1988@gmail.com>
2015-07-11 18:43:23 -0400

    added World object methods

--- electronic_life/e_life.js
+++ electronic_life/e_life.js
@@ -110,3 +110,44 @@ World.prototype.toString = function(){
 function Wall() {}
 
 var world = new World(plan, {"#": Wall, "o": AimlessCritter});
+
+//Animating the critters
+
+Grid.prototype.forEach = function(f, context) {
+      for (var y=0; y< this.height; y++){
+            for (var x=0; x<this.width; x++){
+                  var value = this.space[x + y * this.width];
+                  if (value != null)
+                        f.call(context, value, new Vector(x, y));
+            }
+      }
+};
+
+World.prototype.turn = function() {
+      var acted = [];
+      this.grid.forEach(function(critter, vector){
+            if (critter.act && critter.acted.indexOf(critter) == -1){
+                  acted.push(critter);
+                  this.letAct(critter, vector);
+            }
+      }, this);
+};
+
+World.prototype.letAct = function(critter, vector){
+      var action = critter.act(new View(this, vector));
+      if (action && action.type == 'move') {
+            var dest = this.checkDestination(action, vector);
+            if (dest && this.grid.get(dest) == null){
+                  this.grid.set(vector, null);
+                  this.grid.set(dest, critter);
+            }
+      }
+};
+
+World.prototype.checkDestination = function(action, vector){
+      if (directions.hasOwnProperty(action.direction)) {
+            var dest = vector.plus(directions[action.direction]);
+            if(this.grid.isInside(dest))
+                  return dest;
+      }
+};
