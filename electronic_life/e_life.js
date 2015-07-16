050150f559fca3497602c96ac732b774d3c4b182
Jory Schmidt <joryschmidt1988@gmail.com>
2015-07-16 16:12:53 -0400

    added lifelikeworld

--- electronic_life/e_life.js
+++ electronic_life/e_life.js
@@ -207,3 +207,25 @@ WallFlower.prototype.act = function(view){
       }
       return {type: "move", direction: this.dir};
 };
+
+//Let's make a more interesting World using inheritence
+
+function LifelikeWorld(map, legend){
+      World.call(this, map, legend);
+}
+
+LifelikeWorld.prototype = Object.create(World.prototype);
+
+var actionTypes = Object.create(null);
+
+LifelikeWorld.prototype.letAct = function(critter, vector){
+      var action = critter.act(new View(this, vector));
+      var handled = action && action.type in actionTypes 
+            && actionTypes[action.type].call(this, critter, vector, action);
+      if(!handled){
+            critter.energy -= 0.2;
+            if(critter.energy <= 0)
+                  this.grid.set(vector, null);
+      }
+};
+
